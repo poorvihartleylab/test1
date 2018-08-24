b16342da53afb2dba8476fc620b4ef070642ce3a
Hartley Lab <hello@hartleylab.com>
2018-08-24 15:10:08 +0530

    Add some content

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,5 +8,17 @@
 	<input type="text" name="contact">
 	<input type="text" name="contact1">
 	<input type="submit" name="submit" value="Submit">
+	<p>End</p>
+
+	<p>Bitbucket has the following space stations:</p>
+	<p>
+	<b>Earth's Moon</b><br>
+	Headquarters
+	</p>
+	<p>
+	<b>Mars</b><br>
+	Recreation Department
+	</p>
 </body>
 </html>
+
